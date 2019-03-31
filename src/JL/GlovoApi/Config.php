5df2182402486aaf0abc2fc71dd3071799390dc2
David Câmara <davidcamara21@hotmail.com>
2019-03-31 20:12:55 +0100

    Update Config.php

--- src/JL/GlovoApi/Config.php
+++ src/JL/GlovoApi/Config.php
@@ -4,6 +4,6 @@ namespace JL\GlovoApi;
 
 class Config
 {
-    const STAGE_URL = 'http://stageapi.glovoapp.com/cs/partners/v1';
+    const STAGE_URL = 'http://stageapi.glovoapp.com';
     const PROD_ULR = '';
 }
